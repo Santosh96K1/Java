fe20dee734a6137be8968788e1590538d903c95e
Santosh96K1 <santosh1.xworkz@gmail.com>
2024-08-06 16:58:59 +0530

    adding file

---
+++ ArithmeticOperations.java
@@ -0,0 +1,32 @@
+public class ArithmeticOperations
+{
+public static void add()
+{
+int a=10;
+int b=20;
+int c=a+b;
+System.out.println(c);
+}
+public static void sub()
+{
+int a=10;
+int b=20;
+int c=a-b;
+System.out.println(c);
+}
+public static void mul()
+{
+int a=10;
+int b=20;
+int c=a*b;
+System.out.println(c);
+}
+public static void div()
+{
+int a=20;
+int b=10;
+int c=a/b;
+System.out.println(c);
+}
+}
+
